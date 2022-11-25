918ee7501e0635e368c94a540aa4cd0906a9cc66
candymask0712 <candymask0712@gmail.com>
2022-11-26 00:08:40 +0900

    feat: 배포 시 수정 사항 반영 여부 확인 용

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,5 +3,10 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  return <div>메인페이지입니다.</div>;
+  return (
+    <>
+      <div>메인페이지입니다.</div>
+      <div>수정사항 확인용1</div>
+    </>
+  );
 }
